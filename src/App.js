f58c9e52480e161eaa2448457d86118393281c54
Papuna Datunashvili <papunadatunashvili2@gmail.com>
2025-10-07 15:04:14 +0400

    logo change

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo rounded-full" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to <p>Sonico Georgia</p>
         </p>
         <a
           className="App-link"
